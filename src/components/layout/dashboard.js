f7a56825b6c41e16bcc059856f270ab3549aa3c1
Jean Carlos Berrocal Muñoz <berrocal13@gmail.com>
2018-08-29 22:55:04 -0600

    First Version

---
+++ src/components/layout/dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Dashbord extends Component {
+
+  render() {
+    return (
+      <div className="login-form">
+       
+        <h1>dashbord</h1>
+      </div>
+    );
+  }
+}
